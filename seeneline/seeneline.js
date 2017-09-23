48da31c045fc474e955e9eb08626662c0ddd345f
allarviinamae <allar.viinamae@hotmail.com>
2017-09-23 16:56:44 +0300

    Lab work

--- seeneline/seeneline.js
+++ seeneline/seeneline.js
@@ -1,7 +1,7 @@
-var sources = {
+let sources = {
   treeSprite: 'images/tree.png',
   heroSprite: 'images/hero.png',
-  seenSprite: 'images/seen.png'
+  seenSprite: 'images/seen2.png'
 }
 
 //      NB! The function is "onload" and not "onLoad"
@@ -10,9 +10,9 @@ window.onload = function () {
 }
 
 function loadImages (sources, callback) {
-  var images = {}
-  var loadedImages = 0
-  var numOfImages = 0
+  let images = {}
+  let loadedImages = 0
+  let numOfImages = 0
 
   for (var src in sources) {
 //          console.log("Img loaded: " + src);
@@ -32,18 +32,23 @@ function loadImages (sources, callback) {
 
 }
 
-var seeni = 10  //kui palju on tarvis korjata
-var canvasWidth, canvasHeight, treeImageWidth, treeImageHeight, verticalTrees, horizontalTrees, ctx, pFill,
-  treeImage, hero, seen, collected, keysdown, then
-var startTime = Date.now() //seente korjamise aja leidmiseks
+let seeni = 10  //kui palju on tarvis korjata
+let canvasWidth, canvasHeight, treeImageWidth, treeImageHeight, verticalTrees, horizontalTrees, ctx, pFill,
+  treeImage, hero, collected, keysdown, then
+let startTime = Date.now() //seente korjamise aja leidmiseks
+
+const seenedMax = 5
+let seened = []
 
 randomTrees = []
 
 function initGame (images) {
 
 // Initialize the canvas
-  var canvas = document.getElementById('gamecanvas')
+  let canvas = document.getElementById('gamecanvas')
   ctx = canvas.getContext('2d')
+  canvas.width = innerWidth
+  canvas.height = innerHeight
   canvasWidth = canvas.width
   canvasHeight = canvas.height
   //console.log('canvasWidth,canvasHeight '+canvasWidth+canvasHeight);
@@ -63,21 +68,26 @@ function initGame (images) {
     sprite: images.heroSprite,
     x: Math.floor(canvasWidth / 2),  //to center
     y: Math.floor(canvasHeight / 2),
-    speed: 2 // movement in pixels
+    speed: 5 // movement in pixels
   }
 
-  seen = {
-    sprite: images.seenSprite,
-    x: images.seenSprite.width + (Math.random() * (canvasWidth - 2 * images.seenSprite.width)),
-    y: images.seenSprite.height + (Math.random() * (canvasHeight - 2 * images.seenSprite.height))
-  }
-
-  for (var i = 0; i < 50; i++) {
-    var randomTree = {
-      x: treeImage.width * Math.abs((Math.random() * (treeImageWidth - 2 * treeImage.width))),
-      y: treeImage.height * Math.abs((Math.random() * (treeImageHeight - 2 * treeImage.height)))
+  Array.from(Array(seenedMax).keys()).forEach(() => {
+    let seen = {
+      sprite: images.seenSprite,
+      x: images.seenSprite.width + (Math.random() * (canvasWidth - 2 * images.seenSprite.width)),
+      y: images.seenSprite.height + (Math.random() * (canvasHeight - 2 * images.seenSprite.height))
     }
-    randomTrees = randomTrees.concat(randomTree)
+
+    seened.push(seen)
+  })
+
+  for (let i = 0; i < 50; i++) {
+    let randomTree = {
+      sprite: images.treeSprite,
+      x: images.treeSprite.width + (Math.random() * (canvasWidth - 2 * images.treeSprite.width)),
+      y: images.treeSprite.height + (Math.random() * (canvasHeight - 2 * images.treeSprite.height))
+    }
+    randomTrees.push(randomTree)
   }
 
   collected = 0
@@ -105,20 +115,21 @@ function initGame (images) {
 
 function drawTrees () {
 
-  for (var i = 0; i < horizontalTrees; i++) {
+  for (let i = 0; i < horizontalTrees; i++) {
     ctx.drawImage(treeImage, i * treeImageWidth, 0)
     ctx.drawImage(treeImage, i * treeImageWidth, canvasHeight - treeImageWidth)
   }
-  for (var j = 1; j < verticalTrees; j++) {
+  for (let j = 1; j < verticalTrees; j++) {
     ctx.drawImage(treeImage, 0, j * treeImageWidth)
     ctx.drawImage(treeImage, canvasWidth - treeImageWidth, j * treeImageWidth)
   }
 
-  for (var i in randomTrees) {
+  for (let i in randomTrees) {
     ctx.drawImage(treeImage, randomTrees[i].x, randomTrees[i].y)
   }
 
 }
+
 // Update game objects
 function update () {
   if (87 in keysDown) { // Player holding up
@@ -147,28 +158,41 @@ function update () {
   }
 
   // Are they touching?
-  if (collision(hero, seen)) {
-    ++collected
-    reset()
-  }
+  seened.forEach(seen => {
+    if (collision(hero, seen)) {
+      ++collected
+      reset(seen)
+    }
+  })
+
+  randomTrees.forEach(tree => {
+    if (collision(hero, tree)) {
+      console.log('Tree collision')
+    } else {
+    }
+  })
 }
 
 function collision (obj1, obj2) {
 
-  return obj2.x <= (obj1.x + obj1.sprite.width) &&
-    obj1.x <= (obj2.x + obj2.sprite.width) &&
-    obj2.y <= (obj1.y + obj1.sprite.height) &&
-    obj1.y <= (obj2.y + obj2.sprite.height)
+  return obj2.x <= (obj1.x + obj1.sprite.width / 2) &&
+    obj1.x <= (obj2.x + obj2.sprite.width / 2) &&
+    obj2.y <= (obj1.y + obj1.sprite.height / 2) &&
+    obj1.y <= (obj2.y + obj2.sprite.height / 2)
 
 }
+
 // Draw everything
 function render () {
   ctx.clearRect(0, 0, canvasWidth, canvasHeight)
   drawTrees()
   ctx.drawImage(hero.sprite, hero.x, hero.y)
-  ctx.drawImage(seen.sprite, seen.x, seen.y)
+  seened.forEach(seen => {
+    ctx.drawImage(seen.sprite, seen.x, seen.y)
+  })
   showScore('Seeni: ' + collected)
 }
+
 function showScore (txt) {
   // Score
   ctx.fillStyle = 'rgb(30, 10, 0)'
@@ -177,19 +201,25 @@ function showScore (txt) {
   ctx.textBaseline = 'top'
   ctx.strokeText(txt, treeImageWidth, treeImageWidth) //text, x,y
 }
-var reset = function () {
+
+let reset = function (seen) {
   //Put hero to the center
-  hero.x = canvasWidth / 2
-  hero.y = canvasHeight / 2
+  // hero.x = canvasWidth / 2
+  // hero.y = canvasHeight / 2
+
   // Throw the mushroom somewhere on the screen randomly
-  do {
-    seen.x = seen.sprite.width + (Math.random() * (canvasWidth - 2 * seen.sprite.width))
-    seen.y = seen.sprite.height + (Math.random() * (canvasHeight - 2 * seen.sprite.height))
-  } while (collision(seen, hero))
+  let mushroomsOnScreen = 0
+
+  if (seen) {
+    do {
+      seen.x = seen.sprite.width + (Math.random() * (canvasWidth - 2 * seen.sprite.width))
+      seen.y = seen.sprite.height + (Math.random() * (canvasHeight - 2 * seen.sprite.height))
+    } while (collision(hero, seen) && mushroomsOnScreen <= seenedMax)
+  }
 }
 
 // The main game loop
-var game = function () {
+let game = function () {
   //var now = Date.now();
   //var delta = now - then;
   if (collected < seeni) {
@@ -201,7 +231,12 @@ var game = function () {
   else {
     //ctx.fillStyle = '#AAEECC';
     //ctx.fillRect(treeImageWidth,treeImageWidth,canvasWidth-2*treeImageWidth,treeImageWidth);
-    var time = Math.round(((Date.now() - startTime) / 1000))
+    let time = Math.round(((Date.now() - startTime) / 1000))
     showScore('Said ' + time + ' sekundiga ' + collected + ' seent!')
   }
+}
+
+// Export multiple modules for tests
+module.exports = {
+  collision
 }
